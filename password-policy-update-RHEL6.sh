624376ac1e3c136571cb083d7980f3d853b6282d
ksphub <31971823+ksphub@users.noreply.github.com>
2021-09-16 20:35:18 +0530

    Create password-policy-update-RHEL6.sh

---
+++ password-policy-update-RHEL6.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sed -i 's/.*pam_cracklib.so.*/password requisite pam_cracklib.so try_first_pass retry=3 type= retry=3 minlen=30 lcredit=-1 ucredit=-1 dcredit=-1 ocredit=-1/' /etc/pam.d/system-auth
+
+sed -i 's/.*pam_cracklib.so.*/password requisite pam_cracklib.so try_first_pass retry=3 type= retry=3 minlen=30 lcredit=-1 ucredit=-1 dcredit=-1 ocredit=-1/' /etc/pam.d/password-auth
+
+sed -i '/pam_cracklib.so/ a password requisite pam_pwhistory.so remember=3 retry=3' /etc/pam.d/system-auth
+
+sed -i '/pam_cracklib.so/ a password requisite pam_pwhistory.so remember=3 retry=3' /etc/pam.d/password-auth
