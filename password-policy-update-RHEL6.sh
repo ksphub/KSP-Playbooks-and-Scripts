fa4f41c863942e29230c7a6c731e14b5d8271524
ksphub <31971823+ksphub@users.noreply.github.com>
2024-06-21 20:55:31 +0530

    Update password-policy-update-RHEL6.sh

--- password-policy-update-RHEL6.sh
+++ password-policy-update-RHEL6.sh
@@ -1,3 +1,4 @@
+#Updated by KSP
 #!/bin/bash
 
 sed -i 's/.*pam_cracklib.so.*/password requisite pam_cracklib.so try_first_pass retry=3 type= retry=3 minlen=30 lcredit=-1 ucredit=-1 dcredit=-1 ocredit=-1/' /etc/pam.d/system-auth
